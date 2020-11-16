d8e497d61b3b01b35f75ab35122049ddcecdc1b9
asulzhuts <Let5Java>
2020-11-16 23:10:57 +0300

    Task4 - Boxing and Unboxing

---
+++ BoxUnbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         short forBoxing = 8;
+         object boxing = forBoxing;
+         sbyte unboxing = Convert.ToSByte(boxing);
+
+         Console.WriteLine(unboxing + "  " + unboxing.GetType());
+        }
+    }
+}
